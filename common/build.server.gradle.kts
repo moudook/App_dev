09266f3fe3200092e66932d0e373cd2ef22451c5
moudook <cs24bt055@iitdh.ac.in>
2026-02-23 12:10:45 +0530

    Fix: Add room-common dependency for server build

--- common/build.server.gradle.kts
+++ common/build.server.gradle.kts
@@ -14,6 +14,7 @@ kotlin {
         commonMain.dependencies {
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+            implementation("androidx.room:room-common:2.6.1")
         }
         commonTest.dependencies {
             implementation(kotlin("test"))
